43285927c9584bffcf5d116d0e5b140773063267
jessicahuffstutler <jessica.huffstutler@gmail.com>
2015-11-02 12:00:54 -0500

    adding Charlie input

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -13,7 +13,17 @@ public class Main {
         stmt.execute("UPDATE players SET health = 50 WHERE name = 'Alice'");
         stmt.execute("DELETE FROM players WHERE name = 'Bob'");
 
-        ResultSet results = stmt.executeQuery("SELECT + FROM players");
+          //BAD
+//        String input = "Charlie"; //pretending someone typed this into a web form
+//        stmt.execute(String.format("INSERT INTO players VALUES ('%s', 100, 10, true)", input)); //this will work but is BAD
+
+        //GOOD way to insert a value that a user typed into a query
+        String input = "Charlie";
+        PreparedStatement stmt2 = conn.prepareStatement("INSERT INTO players VALUES (?, 100, 10, true)");
+        stmt2.setString(1, input); //calling the first value
+        stmt2.execute();
+
+        ResultSet results = stmt.executeQuery("SELECT * FROM players");
         while (results.next()) { //loop that will loop over the select results
             String name = results.getString("name"); //put in column name which is "name" //get column name health, make sure it's a double and store it in variable health
             double health = results.getDouble("health");
